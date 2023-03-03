b4f2c5fcbeb5c889988cf8dcec8e3e6851715a5a
Carlota Ojeda <carlotaojeda@DC17L62L24.local>
2023-03-03 19:10:45 +0100

    arreglando parametro en funciones

--- streamlit/data/get_data.py
+++ streamlit/data/get_data.py
@@ -7,22 +7,22 @@ def get_all_teams_statistics():
 def get_all_teams_names():
     return requests.get(f"http://127.0.0.1:8000/equipos/nombres").json()
 
-def get_all_statistic_per_team():
+def get_all_statistic_per_team(nombres):
     return requests.get(f"http://127.0.0.1:8000/equipos/{nombres}").json()
 
-def get_all_possession_per_team():
+def get_all_possession_per_team(nombres):
     return requests.get(f"http://127.0.0.1:8000/equipos/{nombres}/posesion").json()
 
-def get_all_golesfavor_per_team():
+def get_all_golesfavor_per_team(nombres):
     return requests.get(f"http://127.0.0.1:8000/equipos/{nombres}/goles_favor").json()
 
-def get_all_golescontra_per_team():
+def get_all_golescontra_per_team(nombres):
     return requests.get(f"http://127.0.0.1:8000/equipos/{nombres}/goles_contra").json()
 
-def get_all_shots_per_team():
+def get_all_shots_per_team(nombres):
     return requests.get(f"http://127.0.0.1:8000/equipos/{nombres}/tiros_totales").json()
 
-def get_all_shots_ontarget_per_team():
+def get_all_shots_ontarget_per_team(nombres):
     return requests.get(f"http://127.0.0.1:8000/equipos/{nombres}/iros_puerta").json()
 
 def get_all_statistic_max():
@@ -34,20 +34,20 @@ def get_all_players_statistics():
 def get_all_players_name():
     return requests.get(f"http://127.0.0.1:8000/jugadores/nombres").json()
 
-def get_all_statistic_per_player():
+def get_all_statistic_per_player(nombres):
     return requests.get(f"http://127.0.0.1:8000/jugadores/{nombres}/stats").json()
 
-def get_all_goals_per_player():
+def get_all_goals_per_player(nombres):
     return requests.get(f"http://127.0.0.1:8000/jugadores/{nombres}/goles").json()
 
-def get_all_asistencias_per_player():
+def get_all_asistencias_per_player(nombres):
     return requests.get(f"http://127.0.0.1:8000/jugadores/{nombres}/asistencias").json()
 
-def get_all_yellow_card_per_player():
+def get_all_yellow_card_per_player(nombres):
     return requests.get(f"http://127.0.0.1:8000/jugadores/{nombres}/amarillas").json()
 
-def get_all_red_card_per_player():
+def get_all_red_card_per_player(nombres):
     return requests.get(f"http://127.0.0.1:8000/jugadores/{nombres}/rojas").json()
 
-def get_all_max_statistics_player():
+def get_all_max_statistics_player(nombres):
     return requests.get(f"http://127.0.0.1:8000/jugadores/estadistica/maximo").json()
