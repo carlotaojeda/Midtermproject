e0abdb45927ca73855283a92788fa4faab5e9230
Carlota Ojeda <carlotaojeda@DC17L62L24.local>
2023-03-03 19:11:28 +0100

    comenzar main streamlit

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,6 +1,11 @@
 import streamlit as st
+from page.comparar_jugador import comparar_jugador
 
-st.title("Eurocopa 2020")
 
-with st.sidebar:
-    st.radio('Select one:', ["jugadores", "equipos"])
+columna = st.sidebar.selectbox("elige pagina",["Home" , "Buscar jugador", "Buscar equipo","Comparar jugadores", "Comparar equipos"])
+
+if columna == "Home":
+    st.title ("Eurocopa 2020")
+    
+if columna == "Comparar jugadores":
+    comparar_jugador()
