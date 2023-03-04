1abb2e711ddbc21ff164c9ea52bd31bf88cec221
Carlota Ojeda <carlotaojeda@DC17L62L24.local>
2023-03-04 11:31:36 +0100

    finalizada

---
+++ streamlit/page/search_jugador.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from data.get_data import get_all_players_name, get_all_statistic_per_player
+
+def search_jugador():
+    
+    st.title("Busca un jugador")
+
+    lista_jugadores = get_all_players_name()
+
+    jugador = st.selectbox("Elige un jugador", lista_jugadores)
+
+    stats = get_all_statistic_per_player(jugador)
+
+    # Crear una tabla con los datos
+    table = f"""
+        <table>
+            <tr>
+                <td><b>Equipo:</b></td>
+                <td>{stats['nombre']}</td>
+            </tr>
+            <tr>
+                <td><b>Posesión total:</b></td>
+                <td>{stats['goles']}%</td>
+            </tr>
+            <tr>
+                <td><b>Goles a favor:</b></td>
+                <td>{stats['asistencias']}</td>
+            </tr>
+            <tr>
+                <td><b>Goles en contra:</b></td>
+                <td>{stats['tarjetas_rojas']}</td>
+            </tr>
+            <tr>
+                <td><b>Shots totales:</b></td>
+                <td>{stats['tarjetas_amarillas']}</td>
+            </tr>
+        </table>
+    """
+
+    # Mostrar la tabla utilizando markdown
+    st.markdown(table, unsafe_allow_html=True)
