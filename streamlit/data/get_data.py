a26b037e468e1943a4d45db73aa6dad6facfc516
Carlota Ojeda <carlotaojeda@DC17L62L24.local>
2023-03-04 11:26:29 +0100

    cambiado un nombre equivocado

--- streamlit/data/get_data.py
+++ streamlit/data/get_data.py
@@ -8,22 +8,22 @@ def get_all_teams_names():
     return requests.get(f"http://127.0.0.1:8000/equipos/nombres").json()
 
 def get_all_statistic_per_team(nombres):
-    return requests.get(f"http://127.0.0.1:8000/equipos/{nombres}").json()
+    return requests.get(f"http://127.0.0.1:8000/equipo/{nombres}").json()
 
 def get_all_possession_per_team(nombres):
-    return requests.get(f"http://127.0.0.1:8000/equipos/{nombres}/posesion").json()
+    return requests.get(f"http://127.0.0.1:8000/equipo/{nombres}/posesion").json()
 
 def get_all_golesfavor_per_team(nombres):
-    return requests.get(f"http://127.0.0.1:8000/equipos/{nombres}/goles_favor").json()
+    return requests.get(f"http://127.0.0.1:8000/equipo/{nombres}/goles_favor").json()
 
 def get_all_golescontra_per_team(nombres):
-    return requests.get(f"http://127.0.0.1:8000/equipos/{nombres}/goles_contra").json()
+    return requests.get(f"http://127.0.0.1:8000/equipo/{nombres}/goles_contra").json()
 
 def get_all_shots_per_team(nombres):
-    return requests.get(f"http://127.0.0.1:8000/equipos/{nombres}/tiros_totales").json()
+    return requests.get(f"http://127.0.0.1:8000/equipo/{nombres}/tiros_totales").json()
 
 def get_all_shots_ontarget_per_team(nombres):
-    return requests.get(f"http://127.0.0.1:8000/equipos/{nombres}/iros_puerta").json()
+    return requests.get(f"http://127.0.0.1:8000/equipo/{nombres}/iros_puerta").json()
 
 def get_all_statistic_max():
     return requests.get(f"http://127.0.0.1:8000/estadistica/maximo").json()
@@ -35,19 +35,19 @@ def get_all_players_name():
     return requests.get(f"http://127.0.0.1:8000/jugadores/nombres").json()
 
 def get_all_statistic_per_player(nombres):
-    return requests.get(f"http://127.0.0.1:8000/jugadores/{nombres}/stats").json()
+    return requests.get(f"http://127.0.0.1:8000/jugador/{nombres}/stats").json()
 
 def get_all_goals_per_player(nombres):
-    return requests.get(f"http://127.0.0.1:8000/jugadores/{nombres}/goles").json()
+    return requests.get(f"http://127.0.0.1:8000/jugador/{nombres}/goles").json()
 
 def get_all_asistencias_per_player(nombres):
-    return requests.get(f"http://127.0.0.1:8000/jugadores/{nombres}/asistencias").json()
+    return requests.get(f"http://127.0.0.1:8000/jugador/{nombres}/asistencias").json()
 
 def get_all_yellow_card_per_player(nombres):
-    return requests.get(f"http://127.0.0.1:8000/jugadores/{nombres}/amarillas").json()
+    return requests.get(f"http://127.0.0.1:8000/jugador/{nombres}/amarillas").json()
 
 def get_all_red_card_per_player(nombres):
-    return requests.get(f"http://127.0.0.1:8000/jugadores/{nombres}/rojas").json()
+    return requests.get(f"http://127.0.0.1:8000/jugador/{nombres}/rojas").json()
 
 def get_all_max_statistics_player(nombres):
     return requests.get(f"http://127.0.0.1:8000/jugadores/estadistica/maximo").json()
